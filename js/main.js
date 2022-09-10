0b18a3e8dbeed0f6303c308c5066c016ab3abda0
Davoassassin27 <davosoler@gmail.com>
2022-09-09 21:33:51 -0300

    Elimine archivos innecesarios

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".navbar");
-
-navToggle.addEventListener("click", () => {
-  nav.classList.toggle("navbar__pages");
-});
